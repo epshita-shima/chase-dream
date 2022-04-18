4442e6b0221fcdd9990cd47b92260064cbc8113f
epshita-shima <epshitashima@gmail.com>
2022-04-18 09:14:48 +0600

    add services

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import './Service.css'
 
-const Service = () => {
+const Service = ({ service }) => {
+    const { name, img, description, price, id } = service;
+    // const navigate = useNavigate();
     return (
-        <div>
-            <h2>This is service</h2>
+        <div className='service'>
+            <img src={img} alt="" />
+            <h2>This is service:{name}</h2>
+            <p>Price:{price}</p>
+            <p><small>{description}</small></p>
         </div>
     );
 };
